6fee1e242a1c92eceeb7efeb8872a1308aed8418
6Mouris9 <mourisworks@hotmail.com>
2025-12-09 12:21:43 -0300

    Requirements add and WindowModel att

--- windowModel.py
+++ windowModel.py
@@ -1,18 +1,25 @@
+from tkinter import Entry
 import  customtkinter as ctk
 
 
 
 class Janela():
     window = ctk.CTk()
-    
+    valor = "None"
+    entrada = Entry(window, textvariable="variavel")
+    entrada.grid(row=2,column=2, padx=100,pady=110)
 
     
     def button_callback():
-        print("Buttom pressed")
+        Janela.valor = Janela.entrada.get()
+        print(Janela.valor)
+        return True
+    variavel: str
+    
+    
     
     button = ctk.CTkButton(window, text="CLique para printar", command=button_callback)
     button.grid(row=0,column=0, padx=20,pady=20)
-
 
 janela = Janela()
 janela.window.mainloop()
